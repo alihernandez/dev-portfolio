db070713f2256ce4bc1c27648cee95a65005d9ff
phantomramen <ali.hernandez9two@gmail.com>
2021-06-23 15:54:05 -0600

    bad server request 400

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import NavBar from "./components/NavBar/NavBar";
 import Footer from './components/Footer/Footer';
 import resumeData from './components/ResumeData/resumeData';
 import ContactMe from './components/ContactForm/Contact';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
